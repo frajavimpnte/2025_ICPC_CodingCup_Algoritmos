9063d4eb3f93d046131c2fd7a8af588f75bbdd65
frajavimopu <frajavimopu@gmail.com>
2025-01-21 11:32:11 -0600

    ICPC intial repository local

---
+++ c++/Test.cpp
@@ -0,0 +1,19 @@
+/* 
+* ITESS-TICS
+* ICPC FJMP
+* compilación java y ejecución
+* 20-enero-2025
+* famontecillo@itess.edu.mx
+*/
+
+// g++ Test.cpp -o Test.exe
+// Test.exe
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello, programmers!!" << endl;
+}
---
+++ java/Test.java
@@ -0,0 +1,24 @@
+// ITESS-TICS
+// ICPC FJMP
+// compilación java y ejecución
+// 20-enero-2025
+// famontecillo@itess.edu.mx
+
+/* 
+* ITESS-TICS
+* ICPC FJMP
+* compilación java y ejecución
+* 20-enero-2025
+* famontecillo@itess.edu.mx
+*/
+
+//javac Test.java
+//java Test
+
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, programmers!!!!");
+    }
+}
